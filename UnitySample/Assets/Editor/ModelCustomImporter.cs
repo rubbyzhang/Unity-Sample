1c023a98f681b112afbc20bd9985354d13a5ce9f
rubbyzhang <zhangshaobing517@163.com>
2017-08-20 20:44:31 +0800

    ~

---
+++ UnitySample/Assets/Editor/ModelCustomImporter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+class ModelCustomImporter : AssetPostprocessor
+{
+    public void OnPostprocessModel(GameObject model)
+    {
+        Renderer[] renders = model.GetComponentsInChildren<Renderer>();
+        if (null != renders)
+        {
+            foreach (Renderer render in renders)
+            {
+                render.sharedMaterials = new Material[render.sharedMaterials.Length];
+            }
+        }
+    }
+
+    public void OnPreprocessModel()
+    {
+        ModelImporter importer = assetImporter as ModelImporter;
+        Debug.Log("___________________Name:" + importer.assetPath);
+    }
+}
